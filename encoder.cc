e98cf2ca03cee2312d2ddd015f84b3f0822359c7
Arnab Dey <32412133+arnabdeypolimi@users.noreply.github.com>
2018-05-09 00:43:56 +0200

    encoder with gazebo

---
+++ encoder.cc
@@ -0,0 +1,43 @@
+#include <gazebo/gazebo.hh>
+#include <boost/bind.hpp>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/common/common.hh>
+#include <ignition/math/Vector3.hh>
+#include "gazebo/common/Plugin.hh"
+#include <sstream>
+#include <stdio.h>
+#include <gazebo/physics/Joint.hh> 
+
+
+namespace gazebo
+{
+  class EncoderPlugin : public ModelPlugin
+  {
+    	public: EncoderPlugin();
+    	public: void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
+	{
+		this->model = _parent;
+		this->leftJoint = this->model->GetJoint("left_joint");
+      	this->rightJoint = this->model->GetJoint("right_joint");
+		this->updateConnection = event::Events::ConnectWorldUpdateBegin(
+		std::bind(&EncoderPlugin::OnUpdate, this));
+	}
+      	
+     
+	private: virtual void OnUpdate(){
+		double rightWheel = this->rightJoint->GetVelocity(0);
+		double leftWheel =this->leftJoint->GetVelocity(0);
+		printf("Speed left:%d and right:%d\n",leftWheel,rightWheel);
+	}
+	
+	private: physics::ModelPtr model;
+	private: physics::JointPtr leftJoint;
+	private: physics::JointPtr rightJoint;
+	
+	private: event::ConnectionPtr updateConnection;	
+	
+  };
+GZ_REGISTER_MODEL_PLUGIN(EncoderPlugin)
+}
+
+
