81e1f7feab0a81fdc729459dab585962020ac488
Riccardo Bertoglio <ticio94@hotmail.it>
2018-05-09 14:09:11 +0200

    Added working EncoderPlugin file

--- encoder/EncoderPlugin.cc
+++ encoder/EncoderPlugin.cc
@@ -2,43 +2,42 @@
 #include <boost/bind.hpp>
 #include <gazebo/physics/physics.hh>
 #include <gazebo/common/common.hh>
-#include <ignition/math/Vector3.hh>
-#include "gazebo/common/Plugin.hh"
-#include <sstream>
 #include <stdio.h>
 #include <gazebo/physics/Joint.hh> 
 
 
 namespace gazebo
 {
+	
   class EncoderPlugin : public ModelPlugin
-  {
-    	public: EncoderPlugin();
-    	public: void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
+  {    	
+	  
+    public: void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
 	{
 		this->model = _parent;
-		this->leftJoint = this->model->GetJoint("left_joint");
-      		this->rightJoint = this->model->GetJoint("right_joint");
+		
+		this->leftJoint = this->model->GetJoint("joint_left_wheel");
+      	this->rightJoint = this->model->GetJoint("joint_right_wheel");
+		
 		this->updateConnection = event::Events::ConnectWorldUpdateBegin(
 		std::bind(&EncoderPlugin::OnUpdate, this));
-		printf("inside load method\n");
 	}
       	
      
-	private: virtual void OnUpdate(){
+	private: virtual void OnUpdate()
+	{
 		double rightWheel = this->rightJoint->GetVelocity(0);
 		double leftWheel =this->leftJoint->GetVelocity(0);
-		printf("Speed left:%d and right:%d\n",leftWheel,rightWheel);
+		printf("Speed left:%f and right:%f\n",leftWheel,rightWheel);
 	}
 	
 	private: physics::ModelPtr model;
 	private: physics::JointPtr leftJoint;
 	private: physics::JointPtr rightJoint;
-	
 	private: event::ConnectionPtr updateConnection;	
 	
   };
-GZ_REGISTER_MODEL_PLUGIN(EncoderPlugin)
+  GZ_REGISTER_MODEL_PLUGIN(EncoderPlugin);
 }
 
 
